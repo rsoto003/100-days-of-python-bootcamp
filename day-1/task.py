66271f0403b84e12a609e0eed2efb1ee22cce6f5
rsoto003 <ryansotodev@gmail.com>
2024-12-01 02:22:34 -0800

    variables

--- day-1/task.py
+++ day-1/task.py
@@ -1,7 +1,19 @@
+
 # d1.7
-print("hello everyone!\n\tmy name is ryan.\nwhat is yours?")
+# print("hello everyone!\n\tmy name is ryan.\nwhat is yours?")
 
 # d1.8
-print('hello ' + input('what is your name?'))
+# print('hello ' + input('what is your name?') + '!')
+
+# d1.9
+#name = input('what is your name?')
+#print('hello' + name + ', nice to meet you.')
+#print(len(input('what is your favorite food?')))
+
+username = input('what is your name?')
+length = len(username)
+print(length)
+
+
 
 
