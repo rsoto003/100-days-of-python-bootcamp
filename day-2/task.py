941c1fdc2b85f0af6b1a1c4e549571a363ad7f26
rsoto003 <ryansotodev@gmail.com>
2024-12-02 23:39:53 -0800

    type() func

---
+++ day-2/task.py
@@ -0,0 +1,29 @@
+# d2.14
+# Subscripting
+print("Hello World"[-1])
+
+# Strings
+print("123" + "456")  # Concatenation
+
+# Integers = Whole Numbers
+print(123 + 456)
+
+# Large Integers
+print(123_456 + 789)
+
+# Float = Floating Point Number
+print(3.141592653589793)
+
+# Boolean = True or False
+
+print(len('hello world'))
+
+name = 'ryan'
+large_number = 123_456_789
+large_float = 123_456.123
+boolean_value = True
+
+print(type(boolean_value))
+print(type(name))
+print(type(large_number))
+print(type(large_float))
