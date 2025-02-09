6e406007951cbd1fa023ed374b0b1540445b5013
rsoto003 <ryansotodev@gmail.com>
2025-02-09 00:34:43 -0800

    bmi calculator

--- day-3/nested.py
+++ day-3/nested.py
@@ -1,12 +1,16 @@
 height_of_person_in_line = int(input('what is your height in cm?\t'))
 
-if height_of_person_in_line > 120:
+if height_of_person_in_line >= 120:
     print('great, take a seat')
 
     age_of_person_on_ride = int(input('how old are you?\t'))
-    if age_of_person_on_ride <= 18:
-        print('[TEENAGER PRICING]: that will be $7 please')
+
+    if age_of_person_on_ride < 12:
+        print('$5 please.')
+    elif 12 <= age_of_person_on_ride <= 18:
+        print('$7 please.')
     else:
-        print('[ADULT PRICING]: that will be $12 please')
+        print('$12 please.')
+
 else:
     print('apologies, you cannot take a seat on the ride.')
