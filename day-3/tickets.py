90e5acbd3013b33d0549931a850f5f054e886dad
rsoto003 <ryansotodev@gmail.com>
2025-03-03 00:15:17 -0800

    started extra directory to do additional practice.

--- day-3/tickets.py
+++ day-3/tickets.py
@@ -1,19 +1,37 @@
-print('welcome to the rollercoaster ride')
-height = int(input('what is your height in centimeters?\n'))
+print('welcome to the rollercoaster ride ---')
 
-if height >= 120:
-    print('please proceed to rollercoaster, have fun!')
+height = int(input('what is your height in centimeters?\t\n - '))
+price = 0
+can_ride = False
+
+if height > 120:
+    can_ride = True
 else:
-    parental_guardian = input('do you have a parent or guardian to ride the ride with you?')
-    if parental_guardian == 'yes':
-        print('great! have fun on the ride!')
+    has_parent = input("Do you have a parent to ride the ride with you? Y or N\t\n - ")
+    if has_parent.lower() == 'y':
+        can_ride = True
+
+if can_ride:
+    age = int(input('what is your age?\t\n - '))
+    want_photos = input("would you like your photo to be taken? Y or N\t\n - ")
+
+    if age < 12:
+        price = 5
+    elif age <= 18:
+        price = 7
+    elif age >= 45 and age <= 55:
+        print('The ride is on us!')
     else:
-        print('my apologies, you cannot ride this ride.')
+        price = 12
 
-
-wants_photo = input("Would you like your photo to be taken? Y or N\t")
-
-if wants_photo == 'Y':
-    print("Add +$3 to total.")
+    if want_photos.lower() == 'y':
+        price += 3
 else:
-    print("Great, have a great rest of your day.")
+    print("sorry, you cannot ride the ride.")
+
+
+print(f"Your total is ${price:.2f}")
+
+
+
+
