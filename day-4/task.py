d2c924675f06ae18490c8a09f048fe25ad70e44b
rsoto003 <ryansotodev@gmail.com>
2025-04-20 17:01:05 -0700

    day-4: wrapped up #31: random module

--- day-4/task.py
+++ day-4/task.py
@@ -1,12 +1,18 @@
 import random
 import my_module
 
-print(random.randrange(1, 100))
+random_number_0_to_1 = random.random() * 10
+
+random_num = random.randint(1, 10)
+
+random_float = random.uniform(1, 10)
 
 my_module.my_function()
-print(f"My name is {my_module.first_name} {my_module.last_name}")
+print(f"My name is {my_module.first_name} {my_module.last_name}. Random number is {random_number_0_to_1}")
 
+print(random_num)
 
+print(random_float)
 
 
 
